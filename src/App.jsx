0c27c5b8532ff6715866c0d9281a12ba04324d7f
razeeth11 <razeeth011@gmail.com>
2023-03-01 17:01:06 +0530

    rough

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count , setCount] = useState(0)
+  return (
+    <div className="App">
+      <button onClick={()=>
+         setCount(count-1)
+      }>-</button>
+      Count : {count}
+      <button onClick={()=>
+         setCount(count+1)
+      }>+</button>
+    </div>
+  )
+}
+
+export default App
