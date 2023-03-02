475f034413235da7c17989e43d141bbd58e502f3
razeeth11 <razeeth011@gmail.com>
2023-03-02 15:30:21 +0530

    Board-track

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,55 @@ import './App.css'
 function App() {
 
   const [values,setValues] = useState(Array(9).fill(""))
+  const [XTurn,setXTurn] = useState(true)
+
+  const HandClick = (index)=>{
+    if(!final && values[index] === ""){
+      console.log(index)
+    const copy = [...values];
+    copy[index] = XTurn ? "X" : "O"
+    setValues(copy)
+    setXTurn(!XTurn)
+    }
+  }
+
+  const winner = (values)=>{
+
+    const index = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    for (let i = 0; i < index.length; i++) {
+      const [a,b,c] = index[i];
+    if(values[a] != "" && values[a] === values[b] && values[b] === values[c]){
+      return values[a]
+    }
+  }
+    return null
+  }
+
   return (
     <div>
     <div className="App">
-        { values.map((one,index)=> <GameBox box={one} key={index}/>)}
+        { values.map((one,index)=> <GameBox box={one} key={index}/>)} 
     </div>
     </div>
   )
 }
 
 function GameBox() {
-  const [value,setValue] = useState()
   const style = {
     color : value == "X" ? "green" : "red",
   }
   return (
-    <div>
-    <div style={style} className="content-box" onClick={()=>{
-      setValue("X")
-    }}>
+    <div style={style} className="content-box" onClick={onBoxClick}>
         {value}
-    </div>
     </div>
   )
 }
